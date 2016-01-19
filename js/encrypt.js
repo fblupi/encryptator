7d0ba0a28d6e2182cda36dedb4020e169d1bdad0
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2016-01-19 20:38:19 +0100

    Create replacement map

--- js/encrypt.js
+++ js/encrypt.js
@@ -10,6 +10,36 @@ function encrypt() {
 	// 2nd. Step: Get key for replacement
 	var replaceKey = noRepeated(pass1);
 
+	// 3rd. Step: Get map for replacement with the key
+	var replaceMap = getReplacementMap(replaceKey);
+
 	// Set result
 	$("#encrypterResult").val(result);
+}
+
+/**
+ * Generate a map for replace letters by numbers
+ * @param {string} key - Key for disturb
+ * @return {Object} o - Map for replacemente
+ */
+function getReplacementMap(key) {
+	var o = {};
+	var MIN_CONFLICT = 1;
+	var MAX_CONFLICT = 2;
+	var ABC = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+
+	var sequence = noRepeated(key + " " + ABC);
+	var pos = 0;
+
+	for (var i = 0; i <= MAX_CONFLICT; i++) {
+		for (var j = 1; j <= 10; j++) {
+			var value = parseInt("" + i + j % 10);
+			if (value < MIN_CONFLICT || value > MAX_CONFLICT) {
+				o[sequence[pos]] = value;
+				pos++;
+			}
+		}
+	}
+
+	return o;
 }
