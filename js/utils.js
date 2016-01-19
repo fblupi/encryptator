47e88726dab616a1a58e101f5d311db3ec95c9b3
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2016-01-19 23:13:25 +0100

    Encrypting finished (?)

--- js/utils.js
+++ js/utils.js
@@ -3,7 +3,7 @@
  * @param {string} s - Original string
  * @return {string} o - Inverted string
  */
- function invertString(s) {
+function invertString(s) {
 	var o = '';
 	for (var i = s.length - 1; i >= 0; i--) {
 		o += s[i];
@@ -25,9 +25,30 @@ function noRepeated(s) {
 				repeated = true;
 			}
 		}
-		if(!repeated) {
+		if (!repeated) {
 			o += s[i];
 		}
+	}
+	return o;
+}
+
+/**
+ * Generate a number key from a string with the order in the ABC of each letter of the string. For example HELLO --> 21335
+ * @param {string} s - Original string
+ * @return {string} o - String 
+ */
+function getDisplacementKey(s) {
+	var o = '';
+	for (var i = 0; i < s.length; i++) {
+		var value = 1;
+		for (var j = 0; j < s.length; j++) {
+			if (i != j) {
+				if (s[i] > s[j]) {
+					value++;
+				}
+			}
+		}
+		o += value;
 	}
 	return o;
 }
