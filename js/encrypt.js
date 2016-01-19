c0c5a9fe46284778db81bd2a785b2ae10c7f1447
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2016-01-19 20:51:42 +0100

    Replace step encrypting completed (not tested)

--- js/encrypt.js
+++ js/encrypt.js
@@ -1,3 +1,6 @@
+/**
+ * Get a message and two passwords and encrypt the message
+ */
 function encrypt() {
 	// Get values
 	var message = $("#encrypterMessage").val().toUpperCase();
@@ -13,6 +16,9 @@ function encrypt() {
 	// 3rd. Step: Get map for replacement with the key
 	var replaceMap = getReplacementMap(replaceKey);
 
+	// 4th. Step: Replace letters by numbers
+	result = replaceLettersByNumbers(result, replaceMap);
+
 	// Set result
 	$("#encrypterResult").val(result);
 }
@@ -20,17 +26,15 @@ function encrypt() {
 /**
  * Generate a map for replace letters by numbers
  * @param {string} key - Key for disturb
- * @return {Object} o - Map for replacemente
+ * @return {Object} o - Map for replacement
  */
 function getReplacementMap(key) {
 	var o = {};
 	var MIN_CONFLICT = 1;
 	var MAX_CONFLICT = 2;
 	var ABC = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
-
 	var sequence = noRepeated(key + " " + ABC);
 	var pos = 0;
-
 	for (var i = 0; i <= MAX_CONFLICT; i++) {
 		for (var j = 1; j <= 10; j++) {
 			var value = parseInt("" + i + j % 10);
@@ -40,6 +44,19 @@ function getReplacementMap(key) {
 			}
 		}
 	}
+	return o;
+}
 
+/**
+ * Generate a new string by replacing letters by numbers following a map
+ * @param {string} s - Original string with letters
+ * @param {Object} map - Map with replacements
+ * @return {string} o - New string with numbers
+ */
+function replaceLettersByNumbers(s, map) {
+	var o = '';
+	for (var i = 0; i < s.length; i++) {
+		o += map[s[i]];
+	}
 	return o;
 }
